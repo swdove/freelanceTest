828fbe64e5726270d92b64220740938f27140682
Sean Dove <sdove@schlagergroup.com>
2018-03-21 17:34:02 -0600

    completed tutorial

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -6,17 +6,17 @@
 
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
       <ul class="navbar-nav mr-auto">
-        <li class="nav-item active">
+        <li class="nav-item {{Request::is('/') ? 'active' : ''}}">
           <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{Request::is('about') ? 'active' : ''}}">
           <a class="nav-link" href="/about">About</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item {{Request::is('contact') ? 'active' : ''}}">
                 <a class="nav-link" href="/contact">Contact</a>
               </li>        
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
+        <li class="nav-item {{Request::is('messages') ? 'active' : ''}}">
+          <a class="nav-link " href="/messages">Messages</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
